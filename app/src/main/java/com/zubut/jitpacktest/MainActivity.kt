f56e2225d65c9dc35fde9e1f4585c26022a9c6d9
Alejandro Barba <diegoa.brodri@gmail.com>
2019-01-23 12:08:08 -0600

    init commit

---
+++ app/src/main/java/com/zubut/jitpacktest/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.zubut.jitpacktest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.zubut.jitpack.Util
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        Util.test()
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+}
---
+++ jitpack/src/main/java/com/zubut/jitpack/Util.kt
@@ -0,0 +1,14 @@
+package com.zubut.jitpack
+
+import android.util.Log
+
+/**
+ * @author Alejandro Barba on 1/23/19.
+ */
+class Util {
+    companion object {
+        fun test() {
+            Log.d("LIB", "Test from lib")
+        }
+    }
+}
